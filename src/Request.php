ae3d3fab2f8ceefb3f03275d9c99306517595f47
Yury Gavrilov <yuriy@igavrilov.ru>
2019-08-14 17:23:34 +0300

    add copying to files

--- src/Request.php
+++ src/Request.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * This file is part of ManyChat BaseAPI PHP library.
+ * This file is part of ManyChat API PHP library.
  *
- * ManyChat BaseAPI PHP library is free software: you can redistribute it and/or modify
+ * ManyChat API PHP library is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
  *
- * ManyChat BaseAPI PHP library is distributed in the hope that it will be useful,
+ * ManyChat API PHP library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- *    along with ManyChat BaseAPI PHP library.  If not, see <https://www.gnu.org/licenses/>.
+ *    along with ManyChat API PHP library.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @license GPL
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
