599982568754205b1f807652ec7afe05f010df2b
Yury Gavrilov <yuriy@igavrilov.ru>
2019-09-12 12:05:16 +0300

    MIT license

--- src/Exception/CallMethodNotSucceedException.php
+++ src/Exception/CallMethodNotSucceedException.php
@@ -1,23 +1,10 @@
 <?php
 
 /**
- * This file is part of ManyChat API PHP library.
+ * ManyChat API PHP library
  *
- * ManyChat API PHP library is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- * ManyChat API PHP library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- *    along with ManyChat API PHP library.  If not, see <https://www.gnu.org/licenses/>.
- *
- * @license GPL
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright 2019 ManyChat, Inc.
+ * @license https://opensource.org/licenses/MIT The MIT License
  */
 
 namespace ManyChat\Exception;
